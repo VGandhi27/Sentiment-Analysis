fa8c6b752b467661cbc43dde115e184def98144a
VGiktara27 <curiousvidushi27@gmail.com>
2024-07-15 09:26:20 +0530

    updating pkl file

--- visualisation/views.py
+++ visualisation/views.py
@@ -1,15 +1,18 @@
 import os
 import pickle
 from django.shortcuts import render
-from django.conf import settings
-from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
-
+import json
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 # Load the model
 MODEL_PATH = os.path.join(os.path.dirname(__file__), 'machinelearning/sentiment_model_naive_bayes.pkl')
 MODEL_PATH_lr = os.path.join(os.path.dirname(__file__), 'machinelearning/sentiment_model_lr.pkl')
-
+MODEL_PATH_knn = os.path.join(os.path.dirname(__file__), 'machinelearning/sentiment_model_knn.pkl')
+MODEL_PATH_svm = os.path.join(os.path.dirname(__file__), 'machinelearning/sentiment_model_svm.pkl')
+MODEL_PATH_dt = os.path.join(os.path.dirname(__file__), 'machinelearning/sentiment_model_dt.pkl')
 with open(MODEL_PATH, 'rb') as model_file:
     naive_bayes_model = pickle.load(model_file)
 
@@ -20,25 +23,39 @@ def predict_sentiment(text, model):
     # Make a prediction using the loaded model
     prediction = model.predict([text])[0]
     probability = model.predict_proba([text])[0]
-    return prediction, probability
+    probability=(probability * 100).tolist()
+    prob=probability[1]
+    if prediction == 0:
+        sentiment="Negative Sentiment"
+    elif prediction == 1:
+        sentiment="Positive Sentiment"
 
+    else:
+        sentiment="No Sentiment can be detected "
+        prob=0
+    print(f'prediction--{prediction}, probability--{probability},prediction---{prediction},sentiment={sentiment}')
+    return prediction, probability,sentiment
 
+@csrf_exempt
 def sentiment_analysis_view(request):
-    context={}
+    context = {}
     if request.method == 'POST':
         user_input = request.POST.get('user_input', '')
         print(f'user_input--{user_input}')
         # Assuming predict_sentiment returns predictions and probabilities
-        nb_prediction, nb_probability = predict_sentiment(user_input, naive_bayes_model)
-        lr_prediction, lr_probability = predict_sentiment(user_input, lr_model)
+        nb_prediction, nb_probability,nb_sentiment = predict_sentiment(user_input, naive_bayes_model)
+        lr_prediction, lr_probability,lr_sentiment = predict_sentiment(user_input, lr_model)
         
         context = {
             'user_input': user_input,
-            'nb_prediction': nb_prediction * 100,  # Scale to percentage
-            'nb_probability': nb_probability * 100,  # Scale to percentage
-            'lr_prediction': lr_prediction * 100,  # Scale to percentage
-            'lr_probability': lr_probability * 100,  # Scale to percentage
+            'nb_prediction': int(nb_prediction ),  # Scale to percentage and convert to int
+            'nb_probability': (nb_probability ),  # Scale to percentage and convert to list
+            'lr_prediction': int(lr_prediction ),  # Scale to percentage and convert to int
+            'lr_probability': (lr_probability ),  # Scale to percentage and convert to list
+            'lr_sentiment':lr_sentiment,
+            'nb_sentiment':nb_sentiment
         }
+        return JsonResponse(context)
 
-    print(f'context--{context}')
-    return render(request, 'visualisation/home.html',context)
+    data={}
+    return render(request, 'visualisation/home.html', data)
